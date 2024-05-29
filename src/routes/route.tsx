6008b6fd6b49ea91d8b280ae1eafb042f0a04cee
Matheus Castro <matheus.castroks@gmail.com>
2024-05-29 04:26:48 -0300

    adjustment in authState for production

--- src/routes/route.tsx
+++ src/routes/route.tsx
@@ -9,7 +9,7 @@ export default function Routes() {
 
   return (
     <NavigationContainer independent={true}>
-      {!authState?.authenticated ? (
+      {authState?.authenticated ? (
         <>
           <TabRoutes />
           <StatusBar style="auto" translucent animated />
